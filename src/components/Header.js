17f12c0fe25b065d3417ce3e5d36d09c619aaf82
Rob Douma <rdouma81@gmail.com>
2020-07-02 11:26:42 -0400

    Styling completed.

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,13 @@ const Header = (props) => (
     <div className="header">
         <div className="container">
             <h1 className="header__title">{props.title}</h1>
-            <h2 className="header__subtitle">{props.subtitle}</h2>
+            {props.subtitle && <h2 className="header__subtitle">{props.subtitle}</h2>}
         </div>
     </div>
-)
+);
+
+Header.defaultProps = {
+    title: 'Indecision'
+};
 
 export default Header
